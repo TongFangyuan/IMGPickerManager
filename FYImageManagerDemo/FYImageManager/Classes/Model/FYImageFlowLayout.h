5684b26ec95888aa4a43a5825f952e331e336470
tongfy <573682532@qq.com>
2018-04-02 09:56:50 +0800

    update

---
+++ FYImageManagerDemo/FYImageManager/Classes/Model/FYImageFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  FYImageFlowLayout.h
+//  FYImageManagerDemo
+//
+//  Created by tongfy on 2017/11/11.
+//  Copyright © 2017年 tongfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IMGFlowLayout : UICollectionViewFlowLayout
+
+@end
