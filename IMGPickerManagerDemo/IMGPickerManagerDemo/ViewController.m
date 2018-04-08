2c40d43a6c40c81e6e92fc88fee84f55ccffafa4
童方园 <573682532@qq.com>
2018-04-08 21:30:29 +0800

    …

--- IMGPickerManagerDemo/IMGPickerManagerDemo/ViewController.m
+++ IMGPickerManagerDemo/IMGPickerManagerDemo/ViewController.m
@@ -18,6 +18,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor greenColor];
 }
 
 
